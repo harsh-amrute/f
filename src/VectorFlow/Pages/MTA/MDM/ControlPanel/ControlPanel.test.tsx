3db1b25f0ea4b7505b9de72118bde4a210490d04
Rohan Talele <rohan@vectorconsulting.in>
2023-10-25 06:00:17 +0000

    Fixed ControlPanel.test.tsx Linting

--- src/VectorFlow/Pages/MTA/MDM/ControlPanel/ControlPanel.test.tsx
+++ src/VectorFlow/Pages/MTA/MDM/ControlPanel/ControlPanel.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen,waitFor} from '@testing-library/react';
+import { render, fireEvent, screen} from '@testing-library/react';
 
 import ControlPanel from './index';
 import {QueryClientProvider} from '@tanstack/react-query';
@@ -25,14 +25,14 @@ describe('Control Component', () => {
             </QueryClientProvider>
         );
 
-        let viewModifyCard = screen.getAllByTestId('icon-card')[0];
+        const viewModifyCard = screen.getAllByTestId('icon-card')[0];
         expect(viewModifyCard).toBeInTheDocument();
         fireEvent.click(viewModifyCard);
         expect(global.window.location.pathname).toContain('/master-data-management/view-modify');
         fireEvent.click(screen.getAllByTestId('icon-card')[1]);
         fireEvent.click(screen.getAllByTestId('icon-card')[2]);
 
-        let buttonCard = screen.getAllByTestId('button-card');
+        const buttonCard = screen.getAllByTestId('button-card');
         fireEvent.click(buttonCard[0]);
         fireEvent.click(buttonCard[1]);
         fireEvent.click(buttonCard[2]);
