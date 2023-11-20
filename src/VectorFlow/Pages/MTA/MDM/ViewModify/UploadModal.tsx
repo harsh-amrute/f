679d6d5556c04a4be9f3c852b905458ace8ed5b0
Mayur Deopa <mayur@vectorconsulting.in>
2023-11-20 17:18:44 +0530

    fixed code beautification

--- src/VectorFlow/Pages/MTA/MDM/ViewModify/UploadModal.tsx
+++ src/VectorFlow/Pages/MTA/MDM/ViewModify/UploadModal.tsx
@@ -21,101 +21,94 @@ const UploadModal = (props:UploadModalProps)=>{
       onUpload
    }   = props 
 
-const {user} = useUserData()
-return(
-<VFModalCard headerText={"Modification"} headerIcon={"/assets/img/VectorFLOW/NMS/settings.svg"} openModal={openModal} closeModal={onCloseModal} >
-<UploadModalWrapper>
-<UploadModalSection>
- <UploadModalText>
-  <p style={{ color: "#292C2E", marginBottom: '11px'}}><b>Step 1</b></p>
- </UploadModalText>
- <UploadBorderContainer>
-   <UploadModalContent>
-   <TextContent>
-   <img src="../assets/img/manual/excel.png"  height={29} width={29}/>
-    <p>Download selected data </p>
-   </TextContent>
-
-   <UploadFileText>
-      File Name
-   </UploadFileText>
-   <InputWrapper>
-     <UploadModalInput/>
-      <SCManualUploadBtn themeUi={user.user.theme_ui} 
-         onClick={onDownload}
-         style={{
-            height:'40px',
-            width:'93px',
-            borderRadius:'0',
-            display:'flex',
-            alignItems:'center',
-            justifyContent:'center',
-            borderBottomRightRadius:'6px',
-            fontStyle:'normal',
-            fontVariant:'normal',
-            fontWeight: '400',
-            fontSize: '12px',
-            lineHeight: '14px',
-            fontFamily:'Roboto'
-         }}>
-            
-            <img src="/assets/img/VectorFLOW/NMS/download.svg" style={{margin:'5px'}}></img> 
-         Download
-         </SCManualUploadBtn>
-   </InputWrapper>
-</UploadModalContent>
-</UploadBorderContainer>
- </UploadModalSection>
-
-
-
-
- <UploadModalSection style={{margin:0}}>
-  <UploadModalText>
-  <p style={{ color: "#292C2E", marginBottom: '11px'}}><b>Step 2</b></p>
-  </UploadModalText>
- <UploadBorderContainer>
-<UploadModalContent>
-<TextContent>
-<img src="../assets/img/manual/excel.png"  style={{height:'29px', width:'29px'}}/>
-    <p>Drag & Drop your file here</p>
- </TextContent>
- 
- <InputWrapper>
- <SCManualUploadButton style={{height:'40px', width:'105px'}}>
-   <img src="../assets/img/manual/plus.png" width={19} height={19} />
- </SCManualUploadButton>
-     <UploadModalInput/>
-      <SCManualUploadBtn themeUi={user.user.theme_ui} 
-         onClick={onUpload}
-         disabled
-         style={{
-            opacity: 0.34,
-            height:'40px',
-            borderRadius:'0',
-            display:'flex',
-            alignItems:'center',
-            justifyContent:'center',
-            width:'83px',
-            borderBottomRightRadius:'6px',
-            fontStyle:'normal',
-            fontVariant:'normal',
-            fontWeight: '400',
-            fontSize: '12px',
-            lineHeight: '14px',
-            fontFamily:'Roboto'
-         }}>
-         <img src="/assets/img/VectorFLOW/NMS/upload.svg" style={{margin:'6px'}}></img> 
-         Upload
-         </SCManualUploadBtn>
-   </InputWrapper>
-</UploadModalContent>
-   </UploadBorderContainer>
- </UploadModalSection>
-</UploadModalWrapper>
-  
-</VFModalCard>
-    )
+   const {user} = useUserData()
+   return(
+   <VFModalCard headerText={"Modification"} headerIcon={"/assets/img/VectorFLOW/NMS/settings.svg"} openModal={openModal} closeModal={onCloseModal} >
+      <UploadModalWrapper>
+         <UploadModalSection>
+            <UploadModalText>
+               <p style={{ color: "#292C2E", marginBottom: '11px'}}><b>Step 1</b></p>
+            </UploadModalText>
+            <UploadBorderContainer>
+               <UploadModalContent>
+                  <TextContent>
+                     <img src="../assets/img/manual/excel.png"  height={29} width={29}/>
+                     <p>Download selected data </p>
+                  </TextContent>
+                  <UploadFileText>
+                     File Name
+                  </UploadFileText>
+                  <InputWrapper>
+                     <UploadModalInput/>
+                     <SCManualUploadBtn themeUi={user.user.theme_ui} 
+                        onClick={onDownload}
+                        style={{
+                           height:'40px',
+                           width:'93px',
+                           borderRadius:'0',
+                           display:'flex',
+                           alignItems:'center',
+                           justifyContent:'center',
+                           borderBottomRightRadius:'6px',
+                           fontStyle:'normal',
+                           fontVariant:'normal',
+                           fontWeight: '400',
+                           fontSize: '12px',
+                           lineHeight: '14px',
+                           fontFamily:'Roboto'
+                        }}>
+                           
+                        <img src="/assets/img/VectorFLOW/NMS/download.svg" style={{margin:'5px'}}></img> 
+                        Download
+                     </SCManualUploadBtn>
+                  </InputWrapper>
+               </UploadModalContent>
+            </UploadBorderContainer>
+         </UploadModalSection>
+         <UploadModalSection style={{margin:0}}>
+            <UploadModalText>
+               <p style={{ color: "#292C2E", marginBottom: '11px'}}><b>Step 2</b></p>
+            </UploadModalText>
+            <UploadBorderContainer>
+            <UploadModalContent>
+               <TextContent>
+               <img src="../assets/img/manual/excel.png"  style={{height:'29px', width:'29px'}}/>
+                  <p>Drag & Drop your file here</p>
+               </TextContent>     
+               <InputWrapper>
+               <SCManualUploadButton style={{height:'40px', width:'105px'}}>
+                  <img src="../assets/img/manual/plus.png" width={19} height={19} />
+               </SCManualUploadButton>
+                  <UploadModalInput/>
+                     <SCManualUploadBtn themeUi={user.user.theme_ui} 
+                        onClick={onUpload}
+                        disabled
+                        style={{
+                           opacity: 0.34,
+                           height:'40px',
+                           borderRadius:'0',
+                           display:'flex',
+                           alignItems:'center',
+                           justifyContent:'center',
+                           width:'83px',
+                           borderBottomRightRadius:'6px',
+                           fontStyle:'normal',
+                           fontVariant:'normal',
+                           fontWeight: '400',
+                           fontSize: '12px',
+                           lineHeight: '14px',
+                           fontFamily:'Roboto'
+                        }}>
+                        <img src="/assets/img/VectorFLOW/NMS/upload.svg" style={{margin:'6px'}}></img> 
+                        Upload
+                        </SCManualUploadBtn>
+                  </InputWrapper>
+               </UploadModalContent>
+            </UploadBorderContainer>
+         </UploadModalSection>
+      </UploadModalWrapper>
+   </VFModalCard>
+   )
 }
 
 export default UploadModal
