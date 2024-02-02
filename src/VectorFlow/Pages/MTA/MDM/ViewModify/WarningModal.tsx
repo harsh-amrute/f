6baf5b1522631241132714fc2267df7e87e91f73
Akanksha Dwivedi <akankshadwivedi@vectorconsulting.in>
2024-02-02 16:39:56 +0530

    Fixed all button hover state

--- src/VectorFlow/Pages/MTA/MDM/ViewModify/WarningModal.tsx
+++ src/VectorFlow/Pages/MTA/MDM/ViewModify/WarningModal.tsx
@@ -58,7 +58,7 @@ const WarningModal = (props:WarningModalProps) =>{
        <VFModalCard headerText={"Warning"} openModal={true} closeModal={onCloseModal} headerIcon={'/assets/img/VectorFLOW/NMS/warning.svg'} closeIcon={'/assets/img/VectorFLOW/NMS/close-dark.svg'}>
             <p data-testid="warning-test" style={{textAlign:"center", color: "#313131", paddingTop:"36px", fontStyle:"normal", fontVariant:"normal",fontWeight:300,fontSize:"16px",fontFamily:"Roboto"}}>This filter returns <b>{count}</b> records that will be open across mutiple pages.<br/>Do you want to continue?</p> 
             <div style={{display:"flex",gap:"28px", alignItems:"center", justifyContent:"center", paddingTop:"38px", paddingBottom:"36px"}}>
-           <VFButtonOutline color={"gray"} themeUi={user.user.theme_ui} onClick={onFailure}>No</VFButtonOutline>
+           <VFButtonOutline themeUi={user.user.theme_ui} onClick={onFailure}>No</VFButtonOutline>
            <VFButton themeUi={user.user.theme_ui} onClick={onSuccess}>Yes</VFButton>
            </div>
        </VFModalCard>
