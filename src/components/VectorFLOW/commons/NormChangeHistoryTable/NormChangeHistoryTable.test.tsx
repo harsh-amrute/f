859adb268afff819f980309e876c00692de0593a
Akanksha Dwivedi <akankshadwivedi@vectorconsulting.in>
2024-04-11 16:04:18 +0530

    completed Norm Change History Table

---
+++ src/components/VectorFLOW/commons/NormChangeHistoryTable/NormChangeHistoryTable.test.tsx
@@ -0,0 +1,42 @@
+import { screen, render, fireEvent } from "@testing-library/react"
+import NormChangeHistoryTable from "."
+import { QueryClientProvider } from "@tanstack/react-query";
+import { UserDataContext } from "../../../../context";
+import { setupReactQuery } from "../../../../config/react-query-config";
+import {ReactNode} from 'react'
+
+
+const mockFunction = jest.fn()
+const queryClient = setupReactQuery()
+
+const dummyprops = {
+    onGoBack:mockFunction
+}
+
+const contextWrapper = (children: ReactNode) => {
+    return (
+      <QueryClientProvider client={queryClient}>
+            <UserDataContext.Provider
+              value={{
+                user: { user: { theme_ui: "NOIRFUSION" } },
+                changeColorTheme: (color) => {
+                  return color;
+                },
+                isSideBarOpen:true,toggleSideBar:jest.fn
+              }}
+            >
+              {children}
+            </UserDataContext.Provider>
+      </QueryClientProvider>
+    );
+  }
+  
+
+it('Clicks on GoBack button',()=>{
+    render(contextWrapper(<NormChangeHistoryTable {...dummyprops}></NormChangeHistoryTable>))
+        const goBackButton = screen.getByText("Go Back!"); 
+        expect(goBackButton).toBeInTheDocument()
+         fireEvent.click(goBackButton)
+})
+
+
