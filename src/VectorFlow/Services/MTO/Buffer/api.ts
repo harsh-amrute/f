a7fb64d5c78127e44b4a54a85e2270d4c5b7d171
Tarang Mishra <tarang@vectorconsulting.in>
2024-06-11 12:46:35 +0530

    removed vfimage button outline

--- src/VectorFlow/Services/MTO/Buffer/api.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-import axios from 'axios';
-
-export namespace BufferService{
-    export const getBufferData = async (screenType:string) => {
-        return await axios.post(`/GetMasterUIConfiguration`,{screenType:screenType});
-      }
-}
