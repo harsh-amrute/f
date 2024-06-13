62a21e9fd6f2e6ac34334e5e4a9e3e7c8689f95e
Tarang Mishra <tarang@vectorconsulting.in>
2024-06-13 17:34:12 +0530

    lint resolved

--- src/VectorFlow/Pages/MTO/Procurement/Planning/SimulateFullKit/SimulateFullKit.test.tsx
+++ src/VectorFlow/Pages/MTO/Procurement/Planning/SimulateFullKit/SimulateFullKit.test.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
 import SimulateFullKit from './index';
-import { createStore, store } from "../../../../../../redux/store/store";
+import { store } from "../../../../../../redux/store/store";
 import { Provider } from "react-redux";
 import { QueryClientProvider } from '@tanstack/react-query';
 import { setupReactQuery } from '../../../../../../config/react-query-config'
@@ -30,8 +30,8 @@ describe('SimulateFullKit', () => {
         render(contextWrapper(<SimulateFullKit />));
 
 
-        const actionToolBar = document.querySelector('ActionToolBar');
-        const floatingTab = document.querySelector('VFFloatingTab');
+        // const actionToolBar = document.querySelector('ActionToolBar');
+        // const floatingTab = document.querySelector('VFFloatingTab');
 
 
     });
