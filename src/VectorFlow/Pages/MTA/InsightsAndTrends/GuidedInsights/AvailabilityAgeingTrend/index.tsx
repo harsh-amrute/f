074121e867c2e21f945dfc50a61c9bd9c29b060e
Akanksha Dwivedi <akankshadwivedi@vectorconsulting.in>
2024-07-15 12:45:38 +0530

    Updated Data Modification History page

--- src/VectorFlow/Pages/MTA/InsightsAndTrends/GuidedInsights/AvailabilityAgeingTrend/index.tsx
+++ src/VectorFlow/Pages/MTA/InsightsAndTrends/GuidedInsights/AvailabilityAgeingTrend/index.tsx
@@ -137,7 +137,7 @@ const AvailabilityAgeingTrend = ({themeUi}:{themeUi:string}) => {
               fontStyle: "normal",
               fontVariant: "normal",
               fontWeight: 300,
-              fontSize: 15,
+              fontSize: 14,
               fontFamily: "Roboto",
             }}
           >
