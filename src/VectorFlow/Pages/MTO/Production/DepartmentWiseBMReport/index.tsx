abc7670c960e3955f37a9bda84f1ba10df681ebb
Fristan Lobo <Fristan@vectorconsulting.in>
2024-07-23 16:23:47 +0530

    comment resolve

--- src/VectorFlow/Pages/MTO/Production/DepartmentWiseBMReport/index.tsx
+++ src/VectorFlow/Pages/MTO/Production/DepartmentWiseBMReport/index.tsx
@@ -89,8 +89,6 @@ const DptWiseBMReport = () => {
                     isAddFilterButton
                     isExcelExport
                     isWIPCheckBox
-                    onDateChange={() => { console.log('') }}
-                    submitDate={() => { console.log('') }}
                 />
             </BMDepHeaderWraper>
 
