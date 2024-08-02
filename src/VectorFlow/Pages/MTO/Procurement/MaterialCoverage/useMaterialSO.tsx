8b68a1b50592d7d69e0b924fb0117655ba697a0d
Fristan Lobo <Fristan@vectorconsulting.in>
2024-08-02 16:37:01 +0530

    fixed pagination, api param future cov

--- src/VectorFlow/Pages/MTO/Procurement/MaterialCoverage/useMaterialSO.tsx
+++ src/VectorFlow/Pages/MTO/Procurement/MaterialCoverage/useMaterialSO.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
 import { AgGridReactProps } from "ag-grid-react"
 import AvlCellRenderer from '../../Common/AvlCellRenderer';
 import AvailabilityToolTip from "../../../../../VectorFlow/Pages/MTA/InsightsAndTrends/BTR/AvailabilityToolTip";
@@ -13,7 +13,8 @@ import { useGetUIConfigData } from "../../../../../VectorFlow/Services/MTO/Commo
 
 const useMaterialSO = (data: any) => {
     const [orderDetailsData, setOrderDetailsData] = useState<any>();
-
+    const [rowDataCount, setRowDataCount] = useState<number>(0);
+    const [currentPage, setCurrentPage] = useState<number>(1);
 
     // const columnDef = mapMaterialCoverageFieldsToColDefs(HeaderData);
 
@@ -55,7 +56,7 @@ const useMaterialSO = (data: any) => {
     const columnDef = getColumnDefinations(HeaderData, customHeader, extras);
 
     useEffect(() => {
-        getInitialData()
+        getInitialData(currentPage)
     }, [])
 
     const [isLoading, setIsLoading] = useState(false);
@@ -74,138 +75,139 @@ const useMaterialSO = (data: any) => {
         getHeaderData();
     }, [])
 
-    const getInitialData = async () => {
+    const getInitialData = async(currPage:number) => {
 
-        try {
-            setIsLoading(true);
-            toast.dismiss();
-            notifyLoader("Loading data...")
+    try {
+        setIsLoading(true);
+        toast.dismiss();
+        notifyLoader("Loading data...")
 
-            let queryString = '?Color='
-            const colorsArray = Object.keys(data).filter((k: string) => k.startsWith('c'))
-            colorsArray.forEach((s: string, index: number) => {
-                if (index === colorsArray.length - 1) {
-                    queryString += `${data[s]}`
-                }
-                else {
-                    queryString += `${data[s]},`
-                }
-            })
-            queryString += `&KitStatus=${data.kit}&S=${data.S}&E=${data.E}`
+        let queryString = '?Color='
+        const colorsArray = Object.keys(data).filter((k: string) => k.startsWith('c'))
+        colorsArray.forEach((s: string, index: number) => {
+            if (index === colorsArray.length - 1) {
+                queryString += `${data[s]}`
+            }
+            else {
+                queryString += `${data[s]},`
+            }
+        })
+        queryString += `&KitStatus=${data.kit}&S=${data.S}&E=${data.E}&page=${currPage}`
 
-            const someData = await getOpenSODetailsData(queryString);
-            const output = someData.data?.data?.results.map((item: any) => ({
-                ...item,
-                fkapr: ((item.fka / item.oq) * 100).toFixed(2)
-            })
-            )
-            setIsLoading(false);
-            setOrderDetailsData(output)
-            toast.dismiss();
-            notifySuccess("Fetched data successfully!")
-        }
-        catch (e) {
-            setIsLoading(false);
-            notifyError("Failed to load data!")
-        }
+        const someData = await getOpenSODetailsData(queryString);
+        const output = someData.data?.data?.results.map((item: any) => ({
+            ...item,
+            fkapr: ((item.fka / item.oq) * 100).toFixed(2)
+        })
+        )
+        setRowDataCount(someData.data?.data?.count);
+        setIsLoading(false);
+        setOrderDetailsData(output)
+        toast.dismiss();
+        notifySuccess("Fetched data successfully!")
     }
+    catch (e) {
+        setIsLoading(false);
+        notifyError("Failed to load data!")
+    }
+}
 
-    // const LoadData = (data: any) => {
-    //     //console.log('LoadData', data)
-    //     const calculate = data.map((item: any) => ({
-    //         ...item,
-    //         fkapr: ((item.fka / item.oq) * 100).toFixed(2)
-    //     }))
-    //     return calculate;
-    // }
-    // const output = LoadData(OrderDetailsData);
 
-    const autoGroupColumnDef = useMemo(() => {
-        return {
-            minWidth: 250,
-        };
-    }, []);
+const handlePageChangeOnHook = useCallback((param: number) => {
+    setCurrentPage(param);
+    getInitialData(param)
+    // You can add more logic here
+}, []);
 
-    const sideBar = useMemo(() => {
-        return {
-            toolPanels: ['columns'],
-        };
-    }, []);
-    const customCellRenderers = useMemo(() => (
-        {
-            "colorCellRenderer": ColorCellRenderer,
-            "avlCellRenderer": AvlCellRenderer,
-            "availabilityToolTip": AvailabilityToolTip,
-            "customGroupCellRenderer": CustomGroupCellRenderer
-
-        }), []);
-
-    const agGridProps: AgGridReactProps = {
-        tooltipShowDelay: 0,
-        tooltipTrigger: "focus",
-        gridOptions: {
-            rowHeight: 50,
-            getRowStyle: (params: any) => {
-                return {
-                    background: params.node.rowIndex % 2 === 0 ? "#EBEBEB" : "#F7F7F7"
-                };
-            },
-            pagination: true,
-            rowSelection: 'multiple',
-            suppressRowClickSelection: true,
-            enableBrowserTooltips: true,
-            enableRangeSelection: true,
-            components: customCellRenderers,
-            defaultColDef: {
-                filter: 'agTextColumnFilter',
-                floatingFilter: true,
-                cellStyle: {
-                    'text-align': 'center',
-                    'height': '50px',
-                    "font-style": "normal",
-                    "font-variant": "normal",
-                    "font-weight": "300",
-                    "font-size": "20px",
-                    "font-family": "Roboto",
-                    'text-overflow': 'ellipsis',
-                    'white-space': 'nowrap',
-                    'resizable': 'true',
-                },
-
-            },
-
-        },
-
-        sideBar: sideBar,
-        statusBar: {
-            statusPanels: [
-                {
-                    statusPanel: "agTotalRowCountComponent",
-                    align: "left",
-                },
-                {
-                    statusPanel: "agAggregationComponent",
-                    statusPanelParams: {
-                        aggFuncs: ["avg", "sum"],
-                    },
-                },
-            ],
-        },
-        masterDetail: true,
-        detailCellRenderer: DetailCellRenderer,
-        autoGroupColumnDef: autoGroupColumnDef,
-        paginationAutoPageSize: true,
-
-        enterNavigatesVertically: true,
-        enterNavigatesVerticallyAfterEdit: true,
-    };
-
+const autoGroupColumnDef = useMemo(() => {
     return {
-        agGridProps,
-        columnDef,
-        RRRRowData: orderDetailsData,
-        isLoading
-    }
+        minWidth: 250,
+    };
+}, []);
+
+const sideBar = useMemo(() => {
+    return {
+        toolPanels: ['columns'],
+    };
+}, []);
+const customCellRenderers = useMemo(() => (
+    {
+        "colorCellRenderer": ColorCellRenderer,
+        "avlCellRenderer": AvlCellRenderer,
+        "availabilityToolTip": AvailabilityToolTip,
+        "customGroupCellRenderer": CustomGroupCellRenderer
+
+    }), []);
+
+const agGridProps: AgGridReactProps = {
+    tooltipShowDelay: 0,
+    tooltipTrigger: "focus",
+    gridOptions: {
+        rowHeight: 50,
+        getRowStyle: (params: any) => {
+            return {
+                background: params.node.rowIndex % 2 === 0 ? "#EBEBEB" : "#F7F7F7"
+            };
+        },
+        pagination: true,
+        rowSelection: 'multiple',
+        suppressRowClickSelection: true,
+        enableBrowserTooltips: true,
+        enableRangeSelection: true,
+        components: customCellRenderers,
+        defaultColDef: {
+            filter: 'agTextColumnFilter',
+            floatingFilter: true,
+            cellStyle: {
+                'text-align': 'center',
+                'height': '50px',
+                "font-style": "normal",
+                "font-variant": "normal",
+                "font-weight": "300",
+                "font-size": "20px",
+                "font-family": "Roboto",
+                'text-overflow': 'ellipsis',
+                'white-space': 'nowrap',
+                'resizable': 'true',
+            },
+
+        },
+
+    },
+
+    sideBar: sideBar,
+    statusBar: {
+        statusPanels: [
+            {
+                statusPanel: "agTotalRowCountComponent",
+                align: "left",
+            },
+            {
+                statusPanel: "agAggregationComponent",
+                statusPanelParams: {
+                    aggFuncs: ["avg", "sum"],
+                },
+            },
+        ],
+    },
+    masterDetail: true,
+    detailCellRenderer: DetailCellRenderer,
+    autoGroupColumnDef: autoGroupColumnDef,
+    paginationAutoPageSize: true,
+
+    enterNavigatesVertically: true,
+    enterNavigatesVerticallyAfterEdit: true,
+};
+
+return {
+    agGridProps,
+    columnDef,
+    RRRRowData: orderDetailsData,
+    isLoading,
+    rowDataCount: rowDataCount,
+    handlePageChangeOnHook,
+    currentPage: currentPage
+}
 }
 
 export default useMaterialSO;
