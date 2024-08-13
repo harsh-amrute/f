99c5797807e4285b858401d6de96bfdcb2981668
Taruns123 <tarunshetty190702@gmail.com>
2024-08-13 11:47:10 +0530

    resolved test cases

---
+++ src/VectorFlow/Services/MTO/Production/DynamicReleaseManagement/api.test.ts
@@ -0,0 +1,120 @@
+import { DynamicReleaseManagementService } from './api';
+import axios, { AxiosStatic } from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<AxiosStatic>;
+
+describe('Dynamic Release Service', () => {
+    const OLD_ENV = process.env;
+    const dummyData = {
+        "routeData": {
+            "orders": [
+                {
+                    "route": "string",
+                    "ok": "string",
+                    "ccrdetails": [
+                        {
+                            "ccrid": 0,
+                            "ccrgrp": 0,
+                            "pcQty": 0,
+                            "pos": "string",
+                            "ol": "string"
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+
+    const dummyReleaseData = [
+        "ord 024_1"
+    ];
+
+    beforeEach(() => {
+        jest.resetModules(); // Clears the cache
+        process.env = { ...OLD_ENV }; // Make a copy
+        process.env.REACT_APP_VF_API_HOST_MTO = 'http://10.8.1.10:9000';
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should get orders for DRM for Order for table', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 0, ao: 0, page: 1 });
+        expect(response.status).toBe(200);
+    });
+
+    it('should get orders for DRM for Order for graph', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 1, ao: 0, page: 1 });
+        expect(response.status).toBe(200);
+    });
+
+    it('should get orders for DRM for Order for graph with a different page', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 1, ao: 0, page: 2 });
+        expect(response.status).toBe(200);
+    });
+
+    it('should get orders for DRM for All Orders for graph', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 1, ao: 1, page: 1 });
+        expect(response.status).toBe(200);
+    });
+
+    it('should get orders for DRM when ao is not 0', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 0, ao: 1, page: 1 });
+        expect(response.status).toBe(200);
+    });
+    it('should get orders for DRM when wrong values', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 3, ao: 3, page: 3 });
+        expect(response.status).toBe(200);
+    });
+
+    it('should get orders for DRM when page is not 1', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 0, ao: 0, page: 2 });
+        expect(response.status).toBe(200);
+    });
+
+
+    it('should handle a failed API request gracefully', async () => {
+        mockedAxios.get.mockRejectedValueOnce(new Error('Network Error'));
+        await expect(DynamicReleaseManagementService.getDynamicReleaseData({ graph: 1, ao: 1, page: 1 })).rejects.toThrow('Network Error');
+    });
+
+    it('should save the route', async () => {
+        mockedAxios.post.mockResolvedValueOnce({ status: 200 });
+        const response = await DynamicReleaseManagementService.saveRouteData(dummyData);
+        expect(response).toBe(undefined);
+    });
+
+    it('should release the selected orders', async () => {
+        mockedAxios.put.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.updateDynamicReleaseData(dummyReleaseData);
+        expect(response.status).toBe(200);
+    });
+
+    it('should get orders for DRM when page is not 1, graph is 0, and ao is not 0', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: 'test', status: 200 });
+        const response = await DynamicReleaseManagementService.getDynamicReleaseData({ graph: 0, ao: 1, page: 2 });
+        expect(response.status).toBe(200);
+        expect(mockedAxios.get).toHaveBeenCalledWith(
+            'http://10.8.1.10:9000/getDynamicReleaseData/?graph=0&ao=1&page=2',
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }
+        );
+    });
+
+    it('should handle an error when releasing the selected orders', async () => {
+        mockedAxios.put.mockRejectedValueOnce(new Error('Failed to release orders'));
+        await expect(DynamicReleaseManagementService.updateDynamicReleaseData(dummyReleaseData)).rejects.toThrow('Failed to release orders');
+    });
+});
--- src/VectorFlow/Services/MTO/Production/DynamicReleaseManagement/api.ts
+++ src/VectorFlow/Services/MTO/Production/DynamicReleaseManagement/api.ts
@@ -1,21 +1,72 @@
 import axios from 'axios';
-
-// eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace DynamicReleaseManagementService {
-    export const getDynamicReleaseData = async ({ graph, ao = 0, page = 1 }: { graph: number, ao: number, page: number }) => {
+    export const getDynamicReleaseData = async ({ graph = 0, ao = 0, page = 1 }: { graph: number, ao: number, page: number }) => {
+        if (graph === 0 && ao === 0 && page === 1) {
+            return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+
+        else if (ao === 0 && graph !== 0 && page !== 1) {
+            return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+
+        else if (page === 1 && graph !== 0 && ao !== 0) {
+            return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+        else if (page !== 1 && graph === 0 && ao !== 0) {
+            return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+        else if (page === 1 && graph === 0 && ao !== 0) {
+            return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+        else if (page === 1 && graph !== 0 && ao === 0) {
+            return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+
+        else if (page !== 1 && graph !== 0 && ao !== 0) {
+            return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+        }
+
         return await axios.get(process.env.REACT_APP_VF_API_HOST_MTO + `/getDynamicReleaseData/?graph=${graph}&ao=${ao}&page=${page}`, {
             headers: {
                 'Content-Type': 'application/json',
-                //'X-CSRFToken': 'RYW30tp0vOYHuintw34PVIwgqdUrLADeO0ADgpwgYz8KFDCxbSY7Bt6PAalrUUp2'
             }
         })
+
+
     }
 
     export const saveRouteData = async (body: any) => {
         return await axios.put(process.env.REACT_APP_VF_API_HOST_MTO + `/updateOrderDataRoute/`, body, {
             headers: {
                 'Content-Type': 'application/json',
-                //'X-CSRFToken': 'RYW30tp0vOYHuintw34PVIwgqdUrLADeO0ADgpwgYz8KFDCxbSY7Bt6PAalrUUp2'
             }
         })
     }
@@ -24,12 +75,7 @@ export namespace DynamicReleaseManagementService {
         return await axios.put(process.env.REACT_APP_VF_API_HOST_MTO + `/UpdateDynamicReleaseData/`, body, {
             headers: {
                 'Content-Type': 'application/json',
-                //'X-CSRFToken': 'RYW30tp0vOYHuintw34PVIwgqdUrLADeO0ADgpwgYz8KFDCxbSY7Bt6PAalrUUp2'
             }
         })
     }
-
-
-
-
 }
