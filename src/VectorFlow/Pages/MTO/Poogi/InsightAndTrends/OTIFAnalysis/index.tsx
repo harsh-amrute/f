62b9d51e338cd140a3a065fe85240e1fea8563af
vishal sharma <vishal.primathon@vectorconsulting.in>
2024-08-14 10:24:27 +0530

    OTIF API Integration

--- src/VectorFlow/Pages/MTO/Poogi/InsightAndTrends/OTIFAnalysis/index.tsx
+++ src/VectorFlow/Pages/MTO/Poogi/InsightAndTrends/OTIFAnalysis/index.tsx
@@ -2,7 +2,6 @@ import { Allotment } from "allotment";
 import { useEffect, useMemo, useRef, useState } from "react";
 import useViewPort from "../../../../../../hooks/useViewPort";
 import MTOActionToolBar from "../../../../../../components/VectorFLOW/commons/MTO/ActionToolBar/MTOActionToolBar";
-import { APIMock } from "./MockData";
 import { ColDef, GridOptions } from "ag-grid-enterprise";
 import OTIFTrendsGraph from "./OTIFTrendsGraph";
 import OTAndIFTrendsGraph from "./OTAndIFTrendsGraph";
@@ -16,17 +15,41 @@ import { getColumnDefinations } from "../../../../../../helpers/utils";
 import ColorCellRenderer from "../../../../../Pages/MTO/Common/ColorRangeCellRenderer";
 import CustomTagTooltip from "./CustomTagTooltip";
 import TagCellToolTip from "./TagCellRenderer/TagCellRenderer";
+// import { useGetFilterData } from "../../../../../../VectorFlow/Services/MTO/Common/CommonFilter";
 import { useGetUIConfigData } from "../../../../../../VectorFlow/Services/MTO/Common/UIConfig";
+// import useFilter from "../../../../../../hooks/useFilter";
+import { useGetOTIFAnalysisData } from "../../../../../../VectorFlow/Services/MTO/Poogi/InsightAndTrends/OTIFAnalysis";
+
+const APIFilterConfig = {
+  filSecVisConfig :  {
+      "Poogi_OTIF_Analysis" : {
+          mjr : true,
+          or: true,
+          res: true,
+          cus: true
+      },
+  }
+};
 
 const OTIFAnalysis = () => {
   const [isGridView, setIsGridView] = useState(false);
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
   const { screenHeight } = useViewPort();
   const [HeaderData, setHeaderData] = useState([{}]);
   const { mutateAsync: getUIConfigData } = useGetUIConfigData()
+  const { mutateAsync: getOTIFAnalysisData } = useGetOTIFAnalysisData()
+  // const { data: filterResponse, /*isLoading*/ } = useGetFilterData()
   const [colDef, setColDef] = useState([{}]);
+  const [filterData, setFilterData] = useState({});
+  const [gridData, setGridData] = useState([]);
+  const [graphData, setGraphData] = useState<any>({});
   const reportName = "OTIFAnalysis";
 
   const gridRef = useRef();
+
+  const toggleFilter = (state: boolean) => {
+    setIsFilterOpen(state);
+  }
 
   const defaultColDef = useMemo<ColDef>(() => {
     return {
@@ -73,14 +96,50 @@ const OTIFAnalysis = () => {
     }
   }
 
+  const getGridData = async (isGraph: any) => {
+    try {
+      const response = await getOTIFAnalysisData(isGraph);
+      setGridData(response.data.data.results);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+  
+  const getGraphData = async (isGraph: any) => {
+    try {
+      const response = await getOTIFAnalysisData(isGraph);
+      setGraphData(response.data.data);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
   useEffect(() => {
     setColumnDef();
-  }, [])
+    getGridData(0);
+    getGraphData(1);
+  }, []);
+
+  // useEffect(() => {
+  //   setFilterData(filterResponse?.data.data)
+  // }, [filterResponse]);
 
 
   useEffect(() => {
     setColDef(getColumnDefinations(HeaderData, colDefCustomizations))
   }, [HeaderData])
+
+  const onApplyFilter = (filter:any)=>{
+    console.log(filter)
+    setIsFilterOpen(false)
+  }
+  const onAddFilter = ()=>{
+    setIsFilterOpen(true)
+  }
+
+  // const {state:currFilter,setState:setCurrFilter} = useFilter(filterData, APIFilterConfig.filSecVisConfig.Poogi_OTIF_Analysis);
 
   return (
     <div>
@@ -90,6 +149,12 @@ const OTIFAnalysis = () => {
         setIsGridView={setIsGridView}
         isChartGridToggle
         isAddFilterButton
+        // isFilterOpen={isFilterOpen}
+        // onAddFilter={onAddFilter}
+        // toggleFilter={toggleFilter}
+        // onApplyFilter={onApplyFilter} 
+        // multiFilter={currFilter}
+        // setMultiFilter={setCurrFilter}
       />
       <HorizontalViewWrapper style={{ marginTop: "20px", marginLeft: '15px' }}>
         {isGridView ? (
@@ -101,7 +166,7 @@ const OTIFAnalysis = () => {
               }}
               defaultColDef={defaultColDef}
               columnDefs={colDef}
-              rowData={APIMock?.grid}
+              rowData={gridData || []}
               tooltipHideDelay={100000}
               tooltipShowDelay={0}
               tooltipMouseTrack={true}
@@ -121,13 +186,12 @@ const OTIFAnalysis = () => {
             <Allotment vertical={false} separator={false}>
               <Allotment.Pane preferredSize={"50%"}>
                 <BTRAllomentSection>
-                  <OTIFTrendsGraph />
+                  <OTIFTrendsGraph graphData={graphData?.otif} />
                 </BTRAllomentSection>
               </Allotment.Pane>
-
               <Allotment.Pane preferredSize={"50%"}>
                 <BTRAllomentSection>
-                  <OTAndIFTrendsGraph />
+                  <OTAndIFTrendsGraph graphData={graphData?.ot_n_if} />
                 </BTRAllomentSection>
               </Allotment.Pane>
             </Allotment>
