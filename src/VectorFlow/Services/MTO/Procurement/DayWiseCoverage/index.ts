6d848fcbb7f3a5667d0545a65730897a0e718666
Jainish Sakhidas <jainish@vectorconsulting.in>
2024-08-14 14:29:31 +0530

    Removed Daywise Coverage Test Cases

--- src/VectorFlow/Services/MTO/Procurement/DayWiseCoverage/index.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from "react-query"
-import { DayWiseCoverageService } from "./api"
-
-export const useUpdateScheduleOrders = () => {
-    return useMutation(async (body: {orders:any}) => {
-        return DayWiseCoverageService.updateScheduleOrders(body)
-    })
-}
-
