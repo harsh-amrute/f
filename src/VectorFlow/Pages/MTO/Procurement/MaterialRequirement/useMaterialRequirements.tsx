263957ea4222431d4cdf024fe7c4defcd801d87c
Fristan Lobo <Fristan@vectorconsulting.in>
2024-08-24 15:33:37 +0530

    fix and test case

--- src/VectorFlow/Pages/MTO/Procurement/MaterialRequirement/useMaterialRequirements.tsx
+++ src/VectorFlow/Pages/MTO/Procurement/MaterialRequirement/useMaterialRequirements.tsx
@@ -17,6 +17,7 @@ import moment from "moment";
 import { VFTableWrapper } from "../../../../../components/VectorFLOW/commons/VFTable/styles";
 import { useGetUIConfigData } from "../../../../../VectorFlow/Services/MTO/Common/UIConfig";
 import VFPagination from "../../Common/VFPagination";
+import { ColorsMTO } from "../../Common/Colors";
 
 
 
@@ -80,7 +81,7 @@ const useMaterialReq = (forDate?: string) => {
     const setColumnDef = async () => {
         try {
             const response = await getUIConfigData(reportName);
-            setHeaderData(response.data.data);
+            setHeaderData(response?.data?.data);
         }
         catch (e) {
             console.log(e);
@@ -128,6 +129,10 @@ const useMaterialReq = (forDate?: string) => {
             wrapHeaderText: true,
 
         },
+        "net_r": {
+            valueFormatter: (params:any) => Math.max(0, Number(params.data.net_r))
+        }
+        
     }
     const [currentTab, setCurrentTab] = useState<VFFloatingTabItemProps>(tabs[0]);
     const ShortageColumns = getColumnDefinations(HeaderData, customHeader)
@@ -295,6 +300,7 @@ const useMaterialReq = (forDate?: string) => {
             defaultExcelExportParams: defaultExcelExportParams,
             excelStyles: excelStyles,
             sideBar: sideBar,
+            
             onCellEditingStopped(event: any) {
                 const field = event.colDef.field;
                 const newValue = event.newValue;
@@ -316,6 +322,7 @@ const useMaterialReq = (forDate?: string) => {
                 });
                 gridRef.current?.api.refreshCells({ force: true });
             }
+            
         };
         switch (currentTab.id) {
             case "sdv":
