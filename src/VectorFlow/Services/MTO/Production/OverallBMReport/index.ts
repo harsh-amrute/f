bd52cc2eb9c3c20f579ece9fc81fff6a2283c7d6
vishal sharma <vishal.primathon@vectorconsulting.in>
2024-08-27 13:05:12 +0530

    Overall BM Reports Filter API

--- src/VectorFlow/Services/MTO/Production/OverallBMReport/index.ts
+++ src/VectorFlow/Services/MTO/Production/OverallBMReport/index.ts
@@ -1,8 +1,12 @@
 import { useMutation } from '@tanstack/react-query'
 import { OverallBMReportService } from './api'
+type OverallBMReportInputType = {
+    page: number,
+    appliedFilters: any
+}
 
 export const useGetOverAllBMReport = () => {
-    return useMutation(async (data: number) => {
+    return useMutation(async (data: OverallBMReportInputType) => {
         return await OverallBMReportService.getOverallBMReportData(data)
     })
 }
