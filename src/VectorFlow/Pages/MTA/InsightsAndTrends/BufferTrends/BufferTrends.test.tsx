2bf055708b2db8ab62d0b66a21eb0052716056a0
Mayur Deopa <mayur@vectorconsulting.in>
2024-08-28 15:14:21 +0530

    Fixed lint error

--- src/VectorFlow/Pages/MTA/InsightsAndTrends/BufferTrends/BufferTrends.test.tsx
+++ src/VectorFlow/Pages/MTA/InsightsAndTrends/BufferTrends/BufferTrends.test.tsx
@@ -1,4 +1,4 @@
-import { screen,render,fireEvent} from "@testing-library/react";
+import { render} from "@testing-library/react";
 import { mockBTGTechData } from "../../../../../mock-data/BufferTrends";
 import { useGetBufferTrendsGraph } from '../../../../Services/MTA/InsightsAndTrends/BufferTrends/index'
 import { setupReactQuery } from "../../../../../config/react-query-config";
