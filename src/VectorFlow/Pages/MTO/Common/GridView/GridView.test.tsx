73410aea3cfa11d6017f0e8c54c5eca83299ccc2
vishal sharma <vishal.primathon@vectorconsulting.in>
2024-09-10 14:13:11 +0530

    test cases added

--- src/VectorFlow/Pages/MTO/Common/GridView/GridView.test.tsx
+++ src/VectorFlow/Pages/MTO/Common/GridView/GridView.test.tsx
@@ -1,24 +1,13 @@
 import React, { ReactNode } from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render,waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import GridView from './';
-import { notifyError, notifySuccess } from '../../../../../helpers/notify';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { UserDataContext } from '../../../../../context';
 import { createStore } from '../../../../../redux/store/store';
 import { setupReactQuery } from '../../../../../config/react-query-config';
-
-// import OverlayLoader from '../../../../../VectorFlow/Pages/MTO/Common/Loader';
-// import VFPagination from '../../../../../components/VectorFLOW/commons/VFPagination';
-
-// Mock the dependencies
-// jest.mock('../../../../../helpers/notify');
-// jest.mock('../../../../../VectorFlow/Pages/MTO/Common/Loader', () => jest.fn(() => <div>Loading...</div>));
-// jest.mock('../../../../../components/VectorFLOW/commons/VFPagination', () => jest.fn(() => <div>Pagination Component</div>));
-// jest.mock('../../../../../components/VectorFLOW/commons/VFTable', () => jest.fn(() => <div>VFTable Component</div>));
-
 
 const queryClient = setupReactQuery();
 
