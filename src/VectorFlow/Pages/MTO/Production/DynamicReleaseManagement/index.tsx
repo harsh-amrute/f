cffd1fe1467ae6e8aca76583e0b237dd7a30e31a
vishal sharma <vishal.primathon@vectorconsulting.in>
2024-09-11 17:41:16 +0530

    Revert Dynamic code

--- src/VectorFlow/Pages/MTO/Production/DynamicReleaseManagement/index.tsx
+++ src/VectorFlow/Pages/MTO/Production/DynamicReleaseManagement/index.tsx
@@ -21,8 +21,7 @@ import { useGetCCRGroupMaster, useGetLineCCRDetails, useGetRouteDetails } from '
 import OverlayLoader from '../../Common/Loader';
 import VFPagination from '../../Common/VFPagination';
 import { GridRef } from '../../../../../VectorFlow/types/MDM';
-import { pagination, UIGridCode } from '../../Common/Enum';
-import { useGetUserUIConfigData, useUpdateUserUIConfigData } from '../../../../../VectorFlow/Services/MTO/Common/UserUIConfig'
+import { pagination } from '../../Common/Enum';
 
 const DynamicReleaseManagement = () => {
   interface InputData {
@@ -50,6 +49,7 @@ const DynamicReleaseManagement = () => {
   const [order_key, setOrder_Key] = useState('');
   const [table1, setTable1] = useState(true);
   const [showReleaseModal, setShowReleaseModal] = useState(false)
+  const { mutateAsync: getDynamicReleaseData, isLoading, isError, isSuccess } = useGetDynamicReleaseData();
   const [currData, setCurrData] = useState<any>([]);
   const [rowData, setRowData] = useState<any>([]);
   const [graphData, setGraphData] = useState<any>([]);
@@ -57,26 +57,8 @@ const DynamicReleaseManagement = () => {
   const [routeNum, setRouteNum] = useState("");
   const [hide, setHide] = useState(false);
   const [showModal, setShowModal] = useState(false)
-  const [message, setMessage] = useState('');
-  const [lineCCR, setLineCCR] = useState();
-  const [columnState, setColumnState] = useState<any>([]);
-  const [isReset, setIsReset] = useState(false);
-  // const [colDef, setColDef] = useState([{}]);
-  const [currentGridRef, setCurrentGridRef] = useState<any>(null);
-  // const [HeaderData, setHeaderData] = useState([{}]);
-  const [route, setRoute] = useState<any>();
-  const [orderKey, setOrderKey] = useState<any>();
   const graph = useRef<any>();
-  
-  // const { mutateAsync: getUIConfigData } = useGetUIConfigData()
-  const { mutateAsync: getDynamicReleaseData, isLoading, isError, isSuccess } = useGetDynamicReleaseData();
-  const { mutateAsync: getRouteDetails, } = useGetRouteDetails();
-  const { mutateAsync: getCCRGroupMaster, } = useGetCCRGroupMaster();
-  const { mutateAsync: getLineCCRDetails } = useGetLineCCRDetails();
-  const { mutateAsync: updateUserUIReportConfigData, isLoading: isUpdateUserConfig } = useUpdateUserUIConfigData();
-  const { mutateAsync: getUserUIReportConfigData, isLoading: isGetUserConfig } = useGetUserUIConfigData();
-
-
+  const [message, setMessage] = useState('');
   const GetData = async (allOrders = 0, page = 1, graph = 1) => {
     if (allOrders) {
       try {
@@ -110,21 +92,9 @@ const DynamicReleaseManagement = () => {
     }
   };
 
-  const setColumnDef = async () => {
-    try {
-        // const response = await getUIConfigData(reportName);
-        // setHeaderData(response.data.data);
-    }
-    catch (e) {
-        console.log(e);
-    }
-  }
-
   useEffect(() => {
     getMastersData();
     GetData();
-    setColumnDef();
-    getUserColumnConfig();
   }, [])
 
 
@@ -467,6 +437,11 @@ const DynamicReleaseManagement = () => {
     setChartOptions({ ...chartoptions, data: finalGraphData })
   }, [finalGraphData])
 
+
+  const { mutateAsync: getRouteDetails, } = useGetRouteDetails();
+  const { mutateAsync: getCCRGroupMaster, } = useGetCCRGroupMaster();
+  const { mutateAsync: getLineCCRDetails } = useGetLineCCRDetails();
+
   const getMastersData = async () => {
     try {
       const ccrGroupMaster = await getCCRGroupMaster();
@@ -487,6 +462,9 @@ const DynamicReleaseManagement = () => {
     }
   };
 
+  const [route, setRoute] = useState<any>();
+  const [orderKey, setOrderKey] = useState<any>();
+
   useEffect(() => {
 
     if (routeNum !== '') {
@@ -494,6 +472,8 @@ const DynamicReleaseManagement = () => {
     }
 
   }, [routeTrigger, orderKey, routeNum])
+
+  const [lineCCR, setLineCCR] = useState();
 
   const getRoute = async (route: any, orderKey: any) => {
     await getMastersData();
@@ -615,97 +595,18 @@ const DynamicReleaseManagement = () => {
           nodesToSelect.push(node);
         }
 
-    });
-    params.api.setNodesSelected({ nodes: nodesToSelect, newValue: true });
-    params.api.autoSizeAllColumns();
-    setCurrentGridRef(refGrid);
-  }
+      });
+      params.api.setNodesSelected({ nodes: nodesToSelect, newValue: true });
 
-  const getUserColumnConfig = async () => {
-    try {
-        const data = await getUserUIReportConfigData({
-        un: user.user.name,
-        rn_id: UIGridCode.ProdDynamicReleaseManagement
-        });
-
-        const newConfig = JSON.parse(data?.data?.data[0]?.columns_settings) || [];
-        setColumnState(newConfig);
-
-        if (!data) {
-        console.error('Failed to apply column state');
-        }
-    } catch (error) {
-        console.error(error);
     }
-}
-    
-
-const handleSaveClick = async () => {
-    try {
-      if(currentGridRef?.current?.api){
-        const config = currentGridRef.current.api.getColumnState();
-
-        const payload = {
-            un: user.user.name,
-            rn_id: UIGridCode.ProdDynamicReleaseManagement,
-            cs: JSON.stringify(config)
-        }
-        await updateUserUIReportConfigData([payload]);
-        await getUserColumnConfig();
-      }
-
-    } catch (error) {
-    console.error(error);
-    }
-}
-
-const handleResetClick = () => {
-    setIsReset(true);
-}
-
-useEffect(()=>{ 
-    if (currentGridRef?.current && columnState?.length) {
-        const result = currentGridRef.current.api.applyColumnState({
-            state: columnState,
-            applyOrder: true
-        });
-        if (!result) {
-            console.error('Failed to apply column state');
-        }
-    }
-});
-
-// useEffect(() => {
-//   const headerDataCopy = JSON.parse(JSON.stringify(HeaderData));
-//   setColDef(getColumnDefinations(headerDataCopy, colDefCustomizations, extras));
-// }, [HeaderData]);
-
-useEffect(() => {
-    if (isReset) {
-        setColumnState(colDefs);
-        setIsReset(false)
-    }else{
-        handleSaveClick();
-    }
-}, [isReset]);
 
   return (
     <>
       <Wrapper>
         {
-          (isLoading || isUpdateUserConfig || isGetUserConfig)  && <OverlayLoader />
+          isLoading && <OverlayLoader />
         }
-        <MTOActionToolBar 
-          comp="FullKitAssignment" 
-          isExcelExport 
-          isAddFilterButton 
-          isReleaseButton 
-          isReleaseButtonDisabled={isReleaseButtonDisabled} 
-          onOrderRelease={onOrderRelease} 
-          onCheckBoxToggle={setAllRows} 
-          handleSaveClick={handleSaveClick}
-          handleResetClick={handleResetClick}
-        />
+        <MTOActionToolBar comp="FullKitAssignment" isExcelExport isAddFilterButton isReleaseButton isReleaseButtonDisabled={isReleaseButtonDisabled} onOrderRelease={onOrderRelease} onCheckBoxToggle={setAllRows} />
 
         <SCTabHeader style={{ marginTop: '5px' }}>
 
