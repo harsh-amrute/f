99c6cfcff5cc3832c754845a5d02351ae78cb0ff
bhakti <bhakti@DEV-BHAKTIP-TR7>
2024-09-24 15:50:40 +0530

    implemented user services

--- src/module-main/pages/auth/login.tsx
+++ src/module-main/pages/auth/login.tsx
@@ -64,7 +64,8 @@ function LoginContainer() {
             const rolePermission = data?.data?.data.roles.permission;
             const isRolePresent =  rolePermission.some((permission:any) => !permission.name.startsWith("IST"));
             // const url = urlPermission.includes("/") && isRolePresent ? "/" : !urlPermission.includes("/") && isRolePresent ? '/master-data-management/control-panel' : urlPermission[0];
-            const url = urlPermission.includes("/") && isRolePresent ? "/" : urlPermission.includes('/supply-chain-intelligence-hub/planning') ? '/supply-chain-intelligence-hub/planning' : urlPermission[0];
+            const url = urlPermission.includes("/") && isRolePresent ? "/" : urlPermission.includes('/supply-chain-intelligence-hub/planning') ? '/supply-chain-intelligence-hub/planning' : 
+                    urlPermission.includes('/production-planning-scheduling/overall-bm-report') ? "/production-planning-scheduling/overall-bm-report" : urlPermission[0];
             console.log(url);
             navigate(url, { replace: true });
             notifySuccess(t("loginPage.notify.success"));
