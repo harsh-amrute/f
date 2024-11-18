02f55642261cd19385aa264891fb060028bbdd51
Rohan Talele <rohan@vectorconsulting.in>
2024-11-18 13:05:13 +0530

    Reverted MDM

--- src/VectorFlow/Pages/MTA/MDM/SavedDrafts/useSavedDrafts.ts
+++ src/VectorFlow/Pages/MTA/MDM/SavedDrafts/useSavedDrafts.ts
@@ -1,16 +1,14 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useDispatch,useSelector } from "react-redux"
 import { useNavigate } from "react-router"
-import {useGetAllDrafts, useDeleteDraft,useGetDraftById,useGetMasterUIConfiguration, useGetDraftCount, useGetMTODrafts, useGetMTODraftById, useGetMTOMasterUIConfiguration } from "../../../../../VectorFlow/Services/MTA/MDM"
+import {useGetAllDrafts, useDeleteDraft,useGetDraftById,useGetMasterUIConfiguration, useGetDraftCount } from "../../../../../VectorFlow/Services/MTA/MDM"
 import { notifyError, notifyPromise, notifySuccess, notifyLoader } from "../../../../../helpers/notify"
 
-import { FILL_MASTERS, SET_DRAFT_ID, SET_RECORD_COUNT, STORE_ALL_MASTERS, TOGGLE_SELECT_MASTER_SCREEN, TOGGLE_UPLOAD_MODAL, UPDATE_ACTIVE_MASTER, UPDATE_DATA_AVAILABILITY_STATUS } from "../../../../../redux/actions/MDM"
+import { FILL_MASTERS, SET_DRAFT_ID, SET_RECORD_COUNT, STORE_ALL_MASTERS, TOGGLE_SELECT_MASTER_SCREEN, UPDATE_ACTIVE_MASTER, UPDATE_DATA_AVAILABILITY_STATUS } from "../../../../../redux/actions/MDM"
 import { createMastersStateFromDraftData, getActionName, mapMasterToMasterState } from "../../../../../helpers/utils"
 import { MDMMasterState } from "../../../../../VectorFlow/types/MDM"
 import type { RootState } from '../../../../../redux/store/store';
 import { toast } from 'react-toastify';
-import { useUserData } from "../../../../../context"
-import MTOErrorWarningCell from "../ViewModify/MTOErrorWarningCell"
 
 const useSavedDrafts = ()=>{
 
@@ -25,190 +23,32 @@ const useSavedDrafts = ()=>{
     const {data,isLoading,refetch} = useGetAllDrafts();
     const {mutateAsync:getDraftCount} = useGetDraftCount();
     const chunkSize = useSelector((state:RootState) => state.mdm.chunkSize)
-    const [allDrafts, setAllDrafts] = useState<any>(data?.data.data);
-    const {mutateAsync: getMtoDrafts} = useGetMTODrafts();
-    const {mutateAsync: getDraftByIdMTO} = useGetMTODraftById();
-    const {mutateAsync: getMTOMasterUIConfiguration} = useGetMTOMasterUIConfiguration();
-    
-    const user = useUserData();
-    
-    const getCombinedMTOData = async()=>{
-        try{
-            // TODO: change the data to userid later now data is available for this
-            const response:any = await getMtoDrafts(user.user.user.id);
-            let concatedData:any = [];
-            if(data){
-                concatedData = [...data.data.data];
-            }
-            response.data.data.results.forEach((draft: any)=>{
-                const newData = {
-                    DraftId: draft.did,
-                    ActionType: draft.at,
-                    LastModifiedDateTime:draft.co,
-                    Masters: draft.mnm,
-                    SearchKeys: draft.sk,
-                    userid: draft.uid,
-                    isMTO: true
-                }
-                concatedData.push(newData);
-            })
-            
-            setAllDrafts(concatedData);
-            
-        }
-        catch(error){
-            console.log(error)
-        }
-    }
-    
-    
-    
-    useEffect(()=>{
-        if(data?.data){
-            getCombinedMTOData();
-        }
-    },[data])
-    
+    const allDrafts = data?.data.data;
+  
+
     const openDeleteModal = (draftId:string)=>{
         setDeleteDraftId(draftId)
         toggleDeleteModal(true)
     }
-    
+
     const closeDeleteModal =()=>toggleDeleteModal(false)
 
-    // export const createMastersStateFromDraftData = (draftData: any[], fields: any[]): any[] => {
-
-    //     const masters: MDMMasterState[] = []
-    //     draftData.map((master) => {
-    //       const existingMaster = fields.find((m: any) => m.id == master.MasterId)
-    //       if (existingMaster) {
-    //         masters.push({
-    //           id: existingMaster.id,
-    //           name: existingMaster.name,
-    //           colDefs: master.GridState.length > 0 ? JSON.parse(master.GridState) : mapMasterToColumnDefs(existingMaster.fields, existingMaster.id),
-    //           rowData: master.DataMaster || [],
-    //           isChecked: true,
-    //           filters: [{
-    //             id: generateRandomId(),
-    //             masterId: existingMaster.id,
-    //             field: '',
-    //             operator: '',
-    //             text: ''
-    //           }],
-    //           progress: master.Status,
-    //           fields: existingMaster.fields
-    //         })
-    //       }
-    //     })
-    //     return masters
-
-
-    //   }
-
-    const convertToColDefs = (data:any) => {
-        return data.map((item:any )=> ({
-          field: item?.key,                    // Use the "key" as the "field"
-          colId: item?.key,                    // Also set "colId" from "key"
-          headerName: item?.displayName,        // Set "headerName" from "displayName"
-          floatingFilter: false,              // Set default values for additional properties
-          wrapText: true,
-          autoHeight: true,
-          editable: true
-        }));
-      };
-    
     const onEditDraft = async(draftDetails:any)=>{
-        let toastId;
-        if(draftDetails.isMTO){
-            
-            try{
-                const res: any = await getDraftByIdMTO(draftDetails.DraftId);
-                dispatch(SET_RECORD_COUNT(res.data.data.count));
-                const draftData:any = res.data.data.results;
-           
+       let toastId;
 
-            const mastersDataRes= await getMTOMasterUIConfiguration();
-            const mastersData = mastersDataRes.data.data[1];
+       try{
         
-            toast.dismiss();
-            
-            const fields = mastersData.fields;
-
-            // draftData
-            // DataMaster gridState MasterId Status
-            // const masterState = createMastersStateFromDraftData(draftData,fields)
-            // const activeMaster = masterState.find((m:MDMMasterState)=>m.progress!=='submitted');
-                // const activeMaster= "dsf";
-
-            const masterState:any = [
-                {   isMTO: true,
-                    "id": "501",
-                    "name": "SKU",
-                    "colDefs": [{checkboxSelection: true},{colId: 'err', field: 'err',cellRenderer: MTOErrorWarningCell, headerName: 'Error'  },...convertToColDefs(fields)],
-                    "rowData": draftData,
-                    "isChecked": true,
-                    "filters": [
-                    ],
-                    "progress": "something",
-                    "fields": [
-                        {
-                            "displayName": "SKUCode",
-                            "key": "sc",
-                            "col_Position": "1",
-                            "visible": true,
-                            "isAdd": true,
-                            "isEdit": false,
-                            "isDownload": true,
-                            "isApplicable": true,
-                            "dataType": "String"
-                        },
-                    ]
-                }
-            ]
-
-            dispatch(TOGGLE_UPLOAD_MODAL(false));
-
-            
-            dispatch(TOGGLE_SELECT_MASTER_SCREEN(false));
-            // dispatch(STORE_ALL_MASTERS(mapMasterToMasterState(fields)))
-            dispatch(FILL_MASTERS(masterState))
-            dispatch(SET_DRAFT_ID(draftDetails.DraftId))
-            dispatch(UPDATE_DATA_AVAILABILITY_STATUS(true))
-            dispatch(UPDATE_ACTIVE_MASTER(0))
-            
-            // dispatch(UPDATE_ACTIVE_MASTER(masterState.indexOf(activeMaster))            // dispatch(UPDATE_ACTIVE_MASTER())
-            navigate(`/master-data-management/control-panel/${draftDetails.ActionType.toLowerCase()}`);
-            toast.dismiss();
-            notifySuccess("Draft Loaded Successfully");
-
-        }
-        catch(error){
-            console.log(error);
-        }
-            
-            // console.log('act name..',draftDetails.ActionType)
-            // navigate(`/master-data-management/control-panel/${draftDetails.ActionType.toLowerCase()}`);
-            // toast.dismiss();
-            // notifySuccess("Draft Loaded Successfully");
-
-
-
-            return;
-        }
-          
-            try{
-                
-                const res:any = await getDraftCount(draftDetails.DraftId);
-                const draftCount = JSON.parse(res.data.recordCount)[0].recordCount;
-                dispatch(SET_RECORD_COUNT(draftCount));
-                let draftDataRaw = [];
-                const payload = {
+        const res:any = await getDraftCount(draftDetails.DraftId);
+        const draftCount = JSON.parse(res.data.recordCount)[0].recordCount;
+        dispatch(SET_RECORD_COUNT(draftCount));
+        let draftDataRaw = [];
+        const payload = {
             pageNumber:1,
             recordsPerPage:chunkSize
         }
-        
+
         toastId = notifyLoader(`Downloading Data 0 / ${draftCount}`)
-        
+
         if(draftCount <= chunkSize){
             const result = await getDraftById({id:draftDetails.DraftId,body:payload});
             draftDataRaw = result.data.data;
@@ -226,9 +66,9 @@ const useSavedDrafts = ()=>{
         toast.dismiss(toastId);
 
         notifyLoader("Getting Draft Ready")
-        
+    
         // toast.update(toastId,{render:"Getting Draft Ready"});
-        
+       
         const draftData:any = [];
         draftDataRaw.forEach((data:any)=>{
         const masterIndex = draftData.findIndex((draftObj:any) => draftObj.MasterId == data.MasterId);
@@ -243,15 +83,15 @@ const useSavedDrafts = ()=>{
             }
             
         })
-        
+ 
         const mastersData= await getMasterUIConfiguration(getActionName(draftDetails.ActionType).value)
-        
+
         toast.dismiss();
-        
+
         const fields = mastersData.data.data
         const masterState = createMastersStateFromDraftData(draftData,fields)
         const activeMaster = masterState.find((m:MDMMasterState)=>m.progress!=='submitted');
-        
+
         dispatch(TOGGLE_SELECT_MASTER_SCREEN(false))
         dispatch(STORE_ALL_MASTERS(mapMasterToMasterState(fields)))
         dispatch(FILL_MASTERS(masterState))
@@ -260,20 +100,19 @@ const useSavedDrafts = ()=>{
             dispatch(UPDATE_ACTIVE_MASTER(masterState.indexOf(activeMaster)))
             dispatch(UPDATE_DATA_AVAILABILITY_STATUS(true))
         }
-
         
         
         navigate(`/master-data-management/control-panel/${getActionName(draftDetails.ActionType).label}`);
         toast.dismiss();
         notifySuccess("Draft Loaded Successfully");
 
-    }catch(error:any){
+       }catch(error:any){
         notifyError(error.message);
         toast.dismiss(toastId)
+       }
     }
-    }
-    
-    
+
+
     
     const onDeleteDraft = async()=>{
         closeDeleteModal();
@@ -296,6 +135,7 @@ const useSavedDrafts = ()=>{
         onDeleteDraft,
         allDrafts,
         isLoading
+        
     }
 }
 
---
+++ src/VectorFlow/Pages/MTA/MDM/TaskStatus/TaskStatus.test.tsx
@@ -0,0 +1,122 @@
+
+import { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { render,screen } from "@testing-library/react"
+
+import { UserDataContext } from "../../../../../context";
+import TaskStatus from ".";
+import { store } from "../../../../../redux/store/store";
+import { setupReactQuery } from "../../../../../config/react-query-config";
+import { useGetTaskDetailDownloadData, useGetTaskStatusData,useGetMasterUIConfiguration } from "../../../../../VectorFlow/Services/MTA/MDM";
+import { getTaskDetailsDownloadDataMockData, getTaskStatusDataMockData,getMasterUIConfigurationMockData } from "../../../../../mock-data/MDM";
+
+jest.mock("../../../../Services/MTA/MDM");
+
+
+jest.mock('react-toastify', () => ({
+  toast: {
+    error: jest.fn(),
+    success:jest.fn()
+  },
+}))
+
+const useGetTaskStatusDataMock = useGetTaskStatusData as jest.MockedFunction<
+   typeof useGetTaskStatusData
+>
+
+const useGetTaskDetailDownloadDataMock = useGetTaskDetailDownloadData as jest.MockedFunction<
+   typeof useGetTaskDetailDownloadData
+>
+
+const useGetMasterUIConfigurationMock = useGetMasterUIConfiguration as jest.MockedFunction<
+   typeof useGetMasterUIConfiguration
+>
+
+
+window.URL.createObjectURL = jest.fn();
+
+
+const useGetTaskDetailDownloadDataMockData = {
+    mutateAsync:()=>{
+        return {data:getTaskDetailsDownloadDataMockData}
+    }
+} 
+
+const useGetMasterUIConfigurationMockData: any = {
+  mutateAsync: () => {
+    return { data: getMasterUIConfigurationMockData };
+  },
+};
+
+const queryClient = setupReactQuery();
+
+const contextWrapper = (children: ReactNode,store:any) => {
+   return (
+     <QueryClientProvider client={queryClient}>
+       <Router>
+         <Provider store={store}>
+           <UserDataContext.Provider
+             value={{
+               user: { user: { theme_ui: "NOIRFUSION" } },
+               changeColorTheme: (color) => {
+                 return color;
+               },
+               isSideBarOpen:true,toggleSideBar:jest.fn
+             }}
+           >
+             {children}
+           </UserDataContext.Provider>
+         </Provider>
+       </Router>
+     </QueryClientProvider>
+   );
+ };
+ 
+
+
+describe("Handles all renders",()=>{
+   global.ResizeObserver = class MockedResizeObserver {
+      observe = jest.fn();
+      unobserve = jest.fn();
+      disconnect = jest.fn();
+    };
+
+   beforeEach(()=>{
+
+       useGetTaskDetailDownloadDataMock.mockImplementation(():any=>{
+         return useGetTaskDetailDownloadDataMockData;
+       })
+       
+       useGetTaskStatusDataMock.mockImplementation(():any=>{
+        return {data: {data:getTaskStatusDataMockData},isLoading:false} 
+       })
+
+       useGetMasterUIConfigurationMock.mockImplementation(()=>{
+        return useGetMasterUIConfigurationMockData
+      })
+   })
+
+   afterEach(()=>{
+    jest.clearAllMocks()
+   })
+
+   it("renders on the dom",()=>{
+    render(contextWrapper(<TaskStatus/>,store))
+   })
+
+   it("renders the loading on the dom",()=>{
+    useGetTaskStatusDataMock.mockImplementation(():any=>{
+        return {data: {data:getTaskStatusDataMockData},isLoading:true} 
+       })
+      render(contextWrapper(<TaskStatus/>,store))
+      expect(screen.getByTestId('loader')).toBeInTheDocument()
+     })
+
+   it('handles all click events',()=>{
+      render(contextWrapper(<TaskStatus/>,store))
+   })
+
+})
