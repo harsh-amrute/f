4666961778a030e474755a2fffb32d29abd62c41
Harshad Raut <harshad@vectorconsulting.in>
2024-12-26 13:00:14 +0530

    bug solved

--- src/VectorFlow/Pages/MTO/Production/OverallBMReport/index.tsx
+++ src/VectorFlow/Pages/MTO/Production/OverallBMReport/index.tsx
@@ -438,8 +438,8 @@ const OverallBmReport = () => {
 
   
   const handleActionChange = (option: any) => {
+    
     setSelectedAction(option);
-
   }
 
   const updateActionAPI = async (action: string, order_ids: any) => {
@@ -574,7 +574,6 @@ const undoClicked = async (props:any,orderId: string) => {
     throw (error)
   }
 };  
-  
   const WIPFilter: any = (
     <>
        <div
@@ -610,7 +609,7 @@ const undoClicked = async (props:any,orderId: string) => {
           themeUi={themeUi}
           placeholder="Select Action"
           value={selectedAction}
-          onActionChange={handleActionChange}
+          onChange={handleActionChange}
         />
       </div>
 
@@ -686,7 +685,8 @@ const undoClicked = async (props:any,orderId: string) => {
          value={
            actionOptions.find((opt) => opt.value === props.data?.oca) || null
         }
-        onActionChange={(option: any) => {
+        onChange={(option: any) => {
+
             onSelectChange(props, option, props.node.rowIndex);
          }}
        />
