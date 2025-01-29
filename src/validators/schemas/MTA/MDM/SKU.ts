0dedefdc2d80b5656af8e5f9f5a7d626a9c9d8a4
Aryan Mishra <aryan@vectorconsulting.in>
2025-01-29 17:29:28 +0530

    Location Rename to WHCode in Selected Filter(BPR) ,Special Character Validation and changes in saved draft

--- src/validators/schemas/MTA/MDM/SKU.ts
+++ src/validators/schemas/MTA/MDM/SKU.ts
@@ -1,9 +1,9 @@
 import Joi from "joi";
-import { commonValidator, generateCommonMessages,defaultJOIOptions,MAX_CODE_LENGTH,MIN_DECIMAL_VAL,MAX_INT_VAL,MAX_NAME_LENGTH,MAX_DECIMAL_VAL,CommonSchema} from "../../../commons";
+import { commonValidator, generateCommonMessages,defaultJOIOptions,MAX_CODE_LENGTH,MIN_DECIMAL_VAL,MAX_INT_VAL,MAX_NAME_LENGTH,MAX_DECIMAL_VAL,CommonSchema, commonValidatorWithSeperator, DescriptorValidator} from "../../../commons";
 
 export const SKUSchema = Joi.object({
-    sc:Joi.string().empty().invalid(null).max(MAX_CODE_LENGTH).custom(commonValidator).required().messages(generateCommonMessages('SKUCode')),
-    sd:Joi.string().empty().max(MAX_NAME_LENGTH).messages(generateCommonMessages("Description")),
+    sc:Joi.string().empty().invalid(null).max(MAX_CODE_LENGTH).custom(commonValidatorWithSeperator).required().messages(generateCommonMessages('SKUCode')),
+    sd:Joi.string().empty().max(MAX_NAME_LENGTH).custom(DescriptorValidator).required().messages(generateCommonMessages("Description")),
     ec:Joi.number().integer().empty().greater(MIN_DECIMAL_VAL).max(MAX_INT_VAL).messages({'number.unsafe':`Elephant Order Capping should be less than ${MAX_DECIMAL_VAL}`}),
     wt:Joi.number().empty().greater(MIN_DECIMAL_VAL).max(MAX_DECIMAL_VAL).messages({'number.unsafe':`Weight should be less than ${MAX_DECIMAL_VAL}`}),
     vm:Joi.number().empty().greater(MIN_DECIMAL_VAL).max(MAX_DECIMAL_VAL).messages({'number.unsafe':`Volume should be less than ${MAX_DECIMAL_VAL}`}),
