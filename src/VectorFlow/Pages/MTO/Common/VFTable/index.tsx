b13b5f14efe8761854f0c30993423e81242b1b38
Harshad Raut <harshad@vectorconsulting.in>
2025-01-29 17:47:05 +0530

    VFTable Changes

--- src/VectorFlow/Pages/MTO/Common/VFTable/index.tsx
+++ src/VectorFlow/Pages/MTO/Common/VFTable/index.tsx
@@ -6,91 +6,95 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 import './styles.css'
 import { useUserData } from "../../../../../context";
 
-
 interface VFTableProps extends AgGridReactProps {
-  height?: string,
-  disableZoomScaling?: boolean
+  height?: string;
+  disableZoomScaling?: boolean;
+  hideStatusBar?: boolean; 
 }
 
-
 const VFTable = forwardRef((props: VFTableProps, ref: any) => {
-
-  const { user } = useUserData()
-
-  const theme = user.user.theme_ui
+  const { user } = useUserData();
+  const theme = user.user.theme_ui;
 
   const getClassName = () => {
     switch (theme) {
       case "NOIRFUSION":
-        return "ag-theme-noir-fusion"
+        return "ag-theme-noir-fusion";
       case "REGALBLAZE":
-        return "ag-theme-regal-blaze"
+        return "ag-theme-regal-blaze";
       case "PUREELEGANCE":
-        return "ag-theme-pure-elegance"
+        return "ag-theme-pure-elegance";
       case "CHARCOALCHIC":
-        return "ag-theme-charcoal-chic"
+        return "ag-theme-charcoal-chic";
       default:
-        return "ag-theme-noir-fusion"
+        return "ag-theme-noir-fusion";
     }
-  }
+  };
 
-  // console.log(props.gridOptions)
+  const defaultGridOptions = {
+    defaultColDef: {
+      filter: "agTextColumnFilter",
+      floatingFilterComponentParams: { suppressFilterButton: false },
+      floatingFilter: true,
+      suppressMenu: false,
+    },
+    sideBar: {
+      toolPanels: [
+        {
+          id: "columns",
+          labelDefault: "Columns",
+          labelKey: "columns",
+          iconKey: "columns",
+          toolPanel: "agColumnsToolPanel",
+        },
+      ],
+    },
+  };
+
+  const gridOptions = {
+    ...defaultGridOptions,
+    ...props.gridOptions,
+    defaultColDef: {
+      ...defaultGridOptions.defaultColDef,
+      ...props.gridOptions?.defaultColDef,
+    },
+  };
+
+  const defaultStatusBar = {
+    statusPanels: [
+      { statusPanel: "agTotalAndFilteredRowCountComponent" },
+      { statusPanel: "agTotalRowCountComponent" },
+      { statusPanel: "agFilteredRowCountComponent" },
+      { statusPanel: "agSelectedRowCountComponent" },
+      { statusPanel: "agAggregationComponent" },
+    ],
+  };
 
   return (
-    <VFTableWrapper className={`${getClassName()} ag-theme-alpine vfwrap`} role={"table"} height={props.height} disableZoomScaling={props.disableZoomScaling}>
+    <VFTableWrapper
+      className={`${getClassName()} ag-theme-alpine vfwrap`}
+      role="table"
+      height={props.height || "auto"}
+      disableZoomScaling={props.disableZoomScaling || false}
+    >
       <AgGridReact
         ref={ref}
         {...props}
-        gridOptions={{
-          ...props.gridOptions,
-          defaultColDef: {
-            ...props.gridOptions?.defaultColDef,
-            filter: "agTextColumnFilter",
-            floatingFilterComponentParams: { suppressFilterButton: false },
-            floatingFilter: true,
-            suppressMenu: false,
-            // wrapHeaderText: true,
-            // autoHeaderHeight: true,
-            // flex: 0,
-          },
-          sideBar: {
-            toolPanels: [
-              {
-                id: 'columns',
-                labelDefault: 'Columns',
-                labelKey: 'columns',
-                iconKey: 'columns',
-                toolPanel: 'agColumnsToolPanel',
-              },
-            ],
-          },
-        }}
-        statusBar={{
-          statusPanels: [
-            { statusPanel: 'agTotalAndFilteredRowCountComponent' },
-            { statusPanel: 'agTotalRowCountComponent' },
-            { statusPanel: 'agFilteredRowCountComponent' },
-            { statusPanel: 'agSelectedRowCountComponent' },
-            { statusPanel: 'agAggregationComponent' }
-          ]
-        }}
+        gridOptions={gridOptions}
+        statusBar={props.hideStatusBar ? undefined : props.statusBar || defaultStatusBar} 
         enableRangeSelection
-        rowHeight={30}
-        suppressMenuHide={false}
-        suppressDragLeaveHidesColumns={true}
-        defaultColDef={
-          {
-            filter: "agTextColumnFilter",
-            floatingFilterComponentParams: { suppressFilterButton: false },
-            floatingFilter: true,
-            suppressMenu: false,
-            ...props?.gridOptions?.defaultColDef,
-            ...props?.defaultColDef,
-            // wrapHeaderText: true,
-            // autoHeaderHeight: true,
-            // flex: 0,
-          }
+        rowHeight={props.rowHeight || 30}
+        suppressMenuHide={props.suppressMenuHide !== undefined ? props.suppressMenuHide : false}
+        suppressDragLeaveHidesColumns={
+          props.suppressDragLeaveHidesColumns !== undefined
+            ? props.suppressDragLeaveHidesColumns
+            : true
         }
+        defaultColDef={{
+          ...defaultGridOptions.defaultColDef,
+          ...props?.gridOptions?.defaultColDef,
+          ...props?.defaultColDef,
+        }}
       />
     </VFTableWrapper>
   );
