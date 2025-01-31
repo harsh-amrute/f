c70ed89c3f93fb95ad6a449652ab9afbccb3aa1e
Harshad Raut <harshad@vectorconsulting.in>
2025-01-31 12:19:49 +0530

    order_type filter fix

--- src/VectorFlow/Pages/MTO/Common/VFCommonFilter/index.tsx
+++ src/VectorFlow/Pages/MTO/Common/VFCommonFilter/index.tsx
@@ -20,6 +20,7 @@ import { AvailabilityFilter, FilterCheckboxAccordian, FilterMultiSelectCheckbox,
 import VFMasterFieldSearch from "../../../../../components/VectorFLOW/commons/VFMasterFieldSearch";
 import { checkValue } from "../../../../../helpers/utils";
 import { InputTypes } from "../Enum";
+import { log } from "node:console";
 
 interface VFCommonFilterProps {
   onApplyFilter: (params: any) => void;
@@ -122,7 +123,6 @@ const VFCommonFilter = (props: VFCommonFilterProps) => {
         }
       }
     }
-
     setOpenStatus(openFilters);
   }, []);
 
@@ -161,7 +161,7 @@ const VFCommonFilter = (props: VFCommonFilterProps) => {
                     </FilterHeader>
                     <FilterWrapper className="drop-down-options">
                       {filterState[category as keyof FilterState]?.filters.map((filter: Filter) => {
-                        
+                        console.log(filter,"this is filter",filter.type,"this is filter type")
                         if (filter?.type === InputTypes.TextCompare || filter?.type === InputTypes.NumberCompare) {
                           return (
                             <FilterComponent data-testid="" style={{ borderTop: "0.5px solid #B7B7B7" }}>
@@ -263,7 +263,8 @@ const VFCommonFilter = (props: VFCommonFilterProps) => {
                           );
                         }
                         
-                        if (filter.type === InputTypes.MultiSelect) {
+                        if (filter.type === InputTypes.MultiSelect && filter.options?.length !== 0) {
+                        
                           return (
                             <FilterComponent
                               style={{
