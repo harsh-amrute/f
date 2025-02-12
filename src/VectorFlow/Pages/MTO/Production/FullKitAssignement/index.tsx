90621f0599fd327714f8f0b7d5a6db60d5617ad8
Taruns123 <tarunshetty190702@gmail.com>
2025-02-12 19:01:00 +0530

    done with the pagination fix

--- src/VectorFlow/Pages/MTO/Production/FullKitAssignement/index.tsx
+++ src/VectorFlow/Pages/MTO/Production/FullKitAssignement/index.tsx
@@ -26,7 +26,7 @@ import AvailabilityCellRenderer from './AvailabilityCellRenderer';
 import useFilter from "../../../../../hooks/useFilter";
 import { useGetFilterData } from '../../../../../VectorFlow/Services/MTO/Common/CommonFilter';
 import { AvailabilityToolTipWrapper } from '../../../../../VectorFlow/Pages/MTA/InsightsAndTrends/BTR/styles';
-import { FilterPageName, UIGridCode } from '../../Common/Enum';
+import { FilterPageName, pagination, UIGridCode } from '../../Common/Enum';
 import { useGetUserUIConfigData, useUpdateUserUIConfigData } from '../../../../../VectorFlow/Services/MTO/Common/UserUIConfig'
 import useColDef from '../../../../../hooks/useColDef';
 
@@ -803,7 +803,7 @@ const FullKitAssignment = () => {
 
       // }}
       />
-      <VFPagination currentPage={currentPage} rowsPerPage={15} selectedRows={1} totalRows={totalRows} handleChangePage={handlePageChange} resetGridRef={currentGridRef} isDisabled={isDisabled}/>
+      <VFPagination currentPage={currentPage} rowsPerPage={pagination.mtoPageSize} selectedRows={1} totalRows={totalRows || 0} handleChangePage={handlePageChange} resetGridRef={currentGridRef} isDisabled={isDisabled}/>
       <Button arrowName={!hide ? "bg_arrow_down" : "bg_arrow_up"} themeUi={themeUi} onClick={() => { setHide(!hide) }}> {hide ? "Show" : "Hide"} Load Chart</Button>
       <div className='chart-wrapper' style={{ width: "100%", flex: !hide ? 1 : 0, overflow: hide ? "hidden":"unset", minHeight: 0, marginBottom: hide ? "0" : "10px", boxShadow: "0px 6px 12px #81818129" }}>
         <AgCharts ref={graph} options={chartoptions} />
