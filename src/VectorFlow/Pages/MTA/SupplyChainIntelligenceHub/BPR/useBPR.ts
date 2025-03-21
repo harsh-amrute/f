0df49ef2fdcbebcdddcd26d5e18734333d8022fb
= <=>
2025-03-21 13:18:31 +0530

    Code review resolved

--- src/VectorFlow/Pages/MTA/SupplyChainIntelligenceHub/BPR/useBPR.ts
+++ src/VectorFlow/Pages/MTA/SupplyChainIntelligenceHub/BPR/useBPR.ts
@@ -160,7 +160,6 @@ const useBPR =()=>{
         if (internalRef && gridState && gridState.columns) {
             const result = internalRef?.api.applyColumnState({ state: gridState.columns, applyOrder: true });
             internalRef?.api.sizeColumnsToFit();
-            internalRef?.api.sizeColumnsToFit();    
             if (!result) {
                 console.error("Failed to apply column state", result);
             }
