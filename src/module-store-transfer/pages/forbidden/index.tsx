326d06585f88085506be1821aa27849983f4bb17
= <=>
2025-03-21 13:15:38 +0530

    Handled 404, 403 exception , logout issue while downloading multiple report simultaniously

--- src/module-store-transfer/pages/forbidden/index.tsx
+++ src/module-store-transfer/pages/forbidden/index.tsx
@@ -1,11 +1,15 @@
 import { SCPageForbidden, SCTextTitle, SCTextContent } from './styles'
 
 const PageForbidden = () => {
+  const params = new URLSearchParams(window.location.search);
+  const URLPermission = params.get("URLPermission");
   return (
     <SCPageForbidden>
       <SCTextTitle>Error 403 - Forbidden</SCTextTitle>
       <SCTextContent>
-        You don’t have permission to access on this server
+        {URLPermission ?
+          <> Contact your server administrator.</>
+          : <>You don’t have permission to access on this server.</>}
       </SCTextContent>
     </SCPageForbidden>
   )
