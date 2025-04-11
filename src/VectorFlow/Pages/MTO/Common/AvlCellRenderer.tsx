1ef29b59c40c8abcdbad05ad1af232fe295828e2
Harshad Raut <harshad@vectorconsulting.in>
2025-04-11 14:44:39 +0530

    Done with Task

--- src/VectorFlow/Pages/MTO/Common/AvlCellRenderer.tsx
+++ src/VectorFlow/Pages/MTO/Common/AvlCellRenderer.tsx
@@ -6,10 +6,21 @@ const AvlCellRenderer = (props: ICellRendererParams) => {
 
     const { user } = useUserData();
 
+    const formatNumber = (num: any) => {
+        if (num >= 10000000) {
+            return (num / 10000000).toFixed(1) + 'Cr';
+        } else if (num >= 100000) {
+            return (num / 100000).toFixed(1) + 'L'; 
+        } else if (num >= 1000) {
+            return (num / 1000).toFixed(1) + 'K'; 
+        } else {
+            return num; 
+        }
+    };
     const themeUi = user?.user?.theme_ui;
     return (
         <BTRAvailabiltyCellRendererWrapper data-testid="avl-cell-renderer">
-            <div style={{ marginRight: 12 }}>{props.data.fka}</div>
+            <div>{formatNumber(props.data.fka)}</div>
             <BTRAvailabiltyCellRenderer value={props.data.fkapr} themeUi={themeUi}/>
         </BTRAvailabiltyCellRendererWrapper>
     )
--- src/VectorFlow/Pages/MTO/Common/FullkitCellRenderer.tsx
+++ src/VectorFlow/Pages/MTO/Common/FullkitCellRenderer.tsx
@@ -7,10 +7,22 @@ const FullkitCellRenderer = (props: ICellRendererParams) => {
     const { user } = useUserData();
     const themeUi = user?.user?.theme_ui;
 
+    const formatNumber = (num: any) => {
+        if (num >= 10000000) {
+            return (num / 10000000).toFixed(1) + 'Cr';
+        } else if (num >= 100000) {
+            return (num / 100000).toFixed(1) + 'L'; 
+        } else if (num >= 1000) {
+            return (num / 1000).toFixed(1) + 'K'; 
+        } else {
+            return num; 
+        }
+    };
+
     return (
         <BTRAvailabiltyCellRendererWrapper data-testid="avl-cell-renderer">
-            <div style={{ marginRight: 12}}>{props.data.fol}</div>
-            <BTRAvailabiltyCellRenderer value={props.data.fol} themeUi={themeUi}/>
+            <div>{formatNumber(props.data.fol)}</div>
+            <BTRAvailabiltyCellRenderer value={props.data.fol} themeUi={themeUi}/>     
         </BTRAvailabiltyCellRendererWrapper>
     )
 }
