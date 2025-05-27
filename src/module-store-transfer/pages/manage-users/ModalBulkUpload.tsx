692f867b81f8a3ef1de3a51707b877b46ad1945c
Taruns123 <tarunshetty190702@gmail.com>
2025-05-27 07:21:46 +0530

    open close functionality implemented

--- src/module-store-transfer/pages/manage-users/ModalBulkUpload.tsx
+++ src/module-store-transfer/pages/manage-users/ModalBulkUpload.tsx
@@ -21,59 +21,114 @@ export default function PermissionHeirarchyCanvas({ heirarchyData,allPermissions
     const [checked, setChecked] = useState<any>([]);
 
 
-    const generateTreeNodesAndEdges=(data: any)=> {
+    const generateTreeNodesAndEdges=(allNodes: any)=> {
 
-        console.log("data....", data);
+        console.log("allNodes...", allNodes);
         const nodes: any[] = [];
         const edges: any[] = [];
         const positionX = 100;
         const positionY = 0;
-    
-        const level1 = Object.keys(data)
-        .map((ele:any)=>{
-            return {ele, id: uuidv4(), isOpen: false};
-        })
-    
-        const level2:any = [];
-        level1?.forEach((ele:any)=>{
-            let newArr:any = [];
-            newArr = [...newArr, ...Object.keys(data[ele])];
-            level2.push(newArr);
-        })
-    
-        let Yinc = 0;
-        level2.forEach((parent:any, index:number) => {
-            
-            parent.forEach((key:any, index2:number) => {
-            const val = uuidv4();
-            nodes.push({
-                id: key+val,
-                type: 'customNode',
-                position: { x: positionX+500 , y: positionY+(Yinc) },
-                data: { label: key, value: false, isOpen: true, index: index2 },
-            })
-            Yinc+=120;
-            edges.push({              
-                id: `e${parent}-${key+val}`,
-                source: level1[index],
-                target: key+val,
+
+        
+        const level1 = Object.keys(allNodes);
+        let level2:string[] = [];
+        let inIndex = level1.length;
+        level1.forEach((key,index) => {
+            const arr = Object.keys(allNodes[key]);
+            const newArr:string[] = [];
+            arr.forEach((item) => {
+                const val = `${index}_${key}>${inIndex}_${item}`;
+                newArr.push(val);
+                inIndex++;
             });
+            level2 = [...level2, ...newArr];
+        })
+
+        const level3: string[] = [];
+        inIndex = level1.length;
+        let inIndex2 = level1.length + level2.length-1;
+        level1.forEach((key,index) => {
+            const arr = Object.keys(allNodes[key]);
+
+            arr.forEach((item) => {
+                allNodes[key][item].forEach((ele:any)=>{
+                    level3.push(`${index}_${key}>${inIndex}_${item}>${inIndex2}_${ele.location_heirarchy_3}`);
+                    inIndex2++;
+                });
+                inIndex++;
+            })
+        })
+        console.log("level1", level1);
+        console.log("level2", level2);
+        console.log("level3", level3);
+
+
+
+        let index = 0;
+        let indexY = 0;
+
+        level1.forEach((key) => {
+            nodes.push({
+                id: index+'_'+key,
+                type: 'customNode',
+                position: { x: positionX , y: positionY+(indexY*200) },
+                data: { label: index+'_'+key, value: true, isOpen: false, index: index }
+            });
+            index++;
+            indexY++;
         });
-        Yinc+= 120;
-        });
-    
-        let val2 = level2.length/level1.length*10+500;
-    
-        level1.forEach((key, index) => {
+        indexY=0;
+
+        level2.forEach((key) => {
+            const parts = key.split('>');
+            const parentId = parts[0];
+            const label = parts[1];
+            if(opened[key.split('_')[0]]==0){
+                console.log("opened", opened[key.split('_')[0]])
+                index++;
+                indexY++;
+                return;
+            }
             nodes.push({
                 id: key,
                 type: 'customNode',
-                isOpen: true,
-                position: { x: positionX , y: positionY+(index )*80+val2},
-                data: { label: key, value: false, isOpen: true },
-            })
-            val2+=500;
+                position: { x: positionX + 400, y: positionY+(indexY*200) },
+                data: { label: key, value: true, isOpen: false, index: level1.length+indexY }
+            });
+            edges.push({ id: `${parentId}-${key}`, source: key.split('>')[0], target: key });
+            console.log("Id level2 parent",  key )
+            index++;
+            indexY++;
         });
+        indexY=0;
+
+        level3.forEach((key, index3) => {
+            const parts = key.split('>');
+            const parentId = parts[0] + '>' + parts[1];
+            const label = parts[2];
+            if(opened[parts[0].split('_')[0]]==0 || opened[parts[1].split('_')[0]]==0){
+                console.log("opened", opened[key.split('_')[0]], opened[key.split('_')[1]])
+                index++;
+                indexY++;
+                return;
+            }
+            nodes.push({
+                id: key,
+                type: 'customNode',
+                position: { x: positionX + 800, y: positionY+(indexY*200) },
+                data: { label: key, value: true, isOpen: false, index: level1.length+ level2.length+ index3 }
+            })
+            console.log("parentId level3", parentId)
+            edges.push({ id: `${parentId}-${key}`, source: key.split('>')[0]+'>'+key.split('>')[1], target: key });
+            index++;
+            indexY++;
+        });
+
+        console.log("nodes....", nodes)
+
+        
+
+
         return {nodes,edges};
     }
 
@@ -110,6 +165,9 @@ export default function PermissionHeirarchyCanvas({ heirarchyData,allPermissions
         );
     };
     
+
+
+
     const nodeTypes = {
         customNode: CustomNode,
     };
@@ -121,10 +179,10 @@ export default function PermissionHeirarchyCanvas({ heirarchyData,allPermissions
 
 
     React.useEffect(() => {
-        const {nodes: generatedNodes, edges: generatedEdges} = generateTreeNodesAndEdges(allPermissions?.[0]?.location_permission);
+        const {nodes: generatedNodes, edges: generatedEdges} = generateTreeNodesAndEdges(allPermissions?.[1]?.location_permission);
         setNodes(generatedNodes);
         setEdges(generatedEdges);
-    }, [allPermissions]);
+    }, [opened]);
     // const nodes = [
     //     { id: '1', type: 'customNode', position: { x: 0, y: 0 }, data: { label: 'Parent Node', value: true } },
     //     { id: '2', type: 'customNode', position: { x: 200, y: 100 }, data: { label: 'Child Node 1', value: true } },
@@ -135,11 +193,41 @@ export default function PermissionHeirarchyCanvas({ heirarchyData,allPermissions
     //     { id: 'e1-2', source: '1', target: '2' },
     //     { id: 'e1-3', source: '2', target: '3' },
     // ];
-
+    
 
     React.useEffect(()=>{
-        console.log("checked.....", checked)
-    },[checked])
+        const allNodes = allPermissions?.[1]?.location_permission
+        const level1 = Object.keys(allNodes);
+        let level2:string[] = [];
+        let inIndex = level1.length;
+        level1.forEach((key,index) => {
+            const arr = Object.keys(allNodes[key]);
+            const newArr:string[] = [];
+            arr.forEach((item) => {
+                const val = `${index}_${key}>${inIndex}_${item}`;
+                newArr.push(val);
+                inIndex++;
+            });
+            level2 = [...level2, ...newArr];
+        })
+
+        const level3: string[] = [];
+        inIndex = level1.length;
+        let inIndex2 = level1.length + level2.length-1;
+        level1.forEach((key,index) => {
+            const arr = Object.keys(allNodes[key]);
+
+            arr.forEach((item) => {
+                allNodes[key][item].forEach((ele:any)=>{
+                    level3.push(`${index}_${key}>${inIndex}_${item}>${inIndex2}_${ele.location_heirarchy_3}`);
+                    inIndex2++;
+                });
+                inIndex++;
+            })
+        })
+
+        setOpened(Array(level1.length + level2.length + level3.length).fill(0));
+    },[allPermissions])
 
 
     
