2fb1f264b2f3247c603460066acb7c81a5e9896f
Vaibhav wagh <vaibhav.wagh@vectorconsulting.in>
2025-07-28 18:20:51 +0530

    changed for indentation

--- src/VectorFlow/Pages/MTA/MDM/ViewModify/useViewModify.ts
+++ src/VectorFlow/Pages/MTA/MDM/ViewModify/useViewModify.ts
@@ -1105,7 +1105,6 @@ const useViewModify = (pageType:string) => {
           dispatch(SYNC_ACTIVE_MASTER_TO_MASTER());
           setDownloadData(true);
           toast.dismiss(toastId);
-          
           if(fromUploadModal){
             setIsUploadButtonDisabled(false);
             notifySuccess(`Data Downloaded Successfully`);
