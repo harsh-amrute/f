9e64fd267477932336e63c6a4c4a91f19631ed96
Akanksha Dwivedi <akankshadwivedi@vectorconsulting.in>
2026-02-11 15:43:13 +0530

    done with CSP Tooltip issue

--- src/VectorFlow/Pages/MTO/Poogi/InsightAndTrends/OTIFAnalysis/OTIFCommon.ts
+++ src/VectorFlow/Pages/MTO/Poogi/InsightAndTrends/OTIFAnalysis/OTIFCommon.ts
@@ -2,14 +2,14 @@ import "./style.css";
 
 export function TooltipRenderer({ datum, yKey }: any) {
   return `
-      <div class="tooltip-content">
-        <div>
-          <div class="tooltip-row">
-            <div class="tooltip-value">
-              ${datum[yKey]}%
-            </div>
-          </div>
+    <div class="tooltip-content">
+      <div class="tooltip-row">
+        <div class="tooltip-value">
+          ${datum[yKey]}%
         </div>
       </div>
-    `;
+
+    </div>
+  `;
 }
+
