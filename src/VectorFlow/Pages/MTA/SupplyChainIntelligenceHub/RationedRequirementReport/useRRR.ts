e319153a0c8fae3cf2c0e2f2c5fb1ef2a51f7f3f
Harshal Wagh <harshal.wagh@vectorconsulting.in>
2026-03-04 16:22:43 +0530

    Tags added

--- src/VectorFlow/Pages/MTA/SupplyChainIntelligenceHub/RationedRequirementReport/useRRR.ts
+++ src/VectorFlow/Pages/MTA/SupplyChainIntelligenceHub/RationedRequirementReport/useRRR.ts
@@ -17,6 +17,7 @@ import { useGetUIConfigData } from "../../../../Services/MTA/Common/UIConfig"
 import { UIColumnConfigName, UserUIColumnConfigName } from "../../../../../helpers/Enum"
 import { useSelector } from "react-redux"
 import { RootState } from "../../../../../redux/store/store"
+import { BPRTagsCellRenderer } from "../BPR/BPRCellRenderers"
 
  
   
@@ -86,7 +87,7 @@ const useRRR =()=>{
     useEffect(() => {
         const getTableState = async () => {
             try {
-                const MappedColumns = getColumnDefinationsMTA(initialColumnState);
+                const MappedColumns = getColumnDefinationsMTA(initialColumnState, CustomHeader);
                   
                 setGridState({
                     charts: [],
@@ -268,10 +269,15 @@ const useRRR =()=>{
         grapCellRenderer:'',
         colorTechCellRenderer:RRRTechColorCellRenderer,
         colorEcoCellRenderer:RRREcoColorCellRenderer,
-        colorDispatchRender:RRRDispatchColorCellRenderer
+        colorDispatchRender:RRRDispatchColorCellRenderer,
+        TagsCellRenderer: BPRTagsCellRenderer,
       }), []);
 
-      
+    const CustomHeader = {
+        Tags: {
+            cellRenderer: 'TagsCellRenderer',
+        },
+    }
   const defaultColDefObject = useMemo(()=>{
     return {
         floatingFilter: true,
