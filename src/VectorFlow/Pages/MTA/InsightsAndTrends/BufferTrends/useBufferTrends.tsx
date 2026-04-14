1c3c673305504d3804106be968676b243ff5b344
Vaibhav wagh <vaibhav.wagh@vectorconsulting.in>
2026-04-14 12:28:09 +0530

    Refactor summary data handling to remove percentage formatting logic in useBufferTrends

--- src/VectorFlow/Pages/MTA/InsightsAndTrends/BufferTrends/useBufferTrends.tsx
+++ src/VectorFlow/Pages/MTA/InsightsAndTrends/BufferTrends/useBufferTrends.tsx
@@ -139,20 +139,12 @@ const useBufferTrends = () => {
         switch(tab.value){
             case 'Absolute': 
                 setCurrentGraphData(graphData?.data?.absolute);
-                setSummaryData(graphData?.data?.summary.map((row: any) =>
-                row.category === 'Percentage'
-                    ? Object.fromEntries(Object.entries(row).map(([k, v]) => k === 'category' ? [k, v] : [k, `${v}%`]))
-                    : row
-            )as any);
+                setSummaryData(graphData?.data?.summary);
                 setAvailability(graphData?.data?.avail);
                 break;
             case 'Percentage':
                 setCurrentGraphData(graphData?.data?.percentage);
-                setSummaryData(graphData?.data?.summary.map((row: any) =>
-                row.category === 'Percentage'
-                    ? Object.fromEntries(Object.entries(row).map(([k, v]) => k === 'category' ? [k, v] : [k, `${v}%`]))
-                    : row
-            )as any);
+                setSummaryData(graphData?.data?.summary);
                 setAvailability(graphData?.data?.avail);
         }
     } 
